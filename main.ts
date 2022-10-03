e8e8b8ef601e46a3d5f5aabbb576b073aa2ce197
timflee <timflee@hotmail.com>
2022-10-03 23:35:07 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    AorB = "A"
-    basic.showString(AorB)
+    transmitPeriod = transmitPeriod * 2
+    radio.sendValue("transmitPeriod", transmitPeriod)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString(AorB)
+	
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    AorB = "B"
-    basic.showString(AorB)
+    transmitPeriod = transmitPeriod / 2
+    radio.sendValue("transmitPeriod", transmitPeriod)
 })
-let AorB = ""
+let transmitPeriod = 0
 radio.setGroup(1)
+transmitPeriod = 1024
+radio.sendValue("transmitPeriod", transmitPeriod)
 basic.forever(function () {
-	
+    while (true) {
+        radio.sendValue("X accel", input.acceleration(Dimension.X))
+        radio.sendValue("Y accel", input.acceleration(Dimension.Y))
+        radio.sendValue("Z accel", input.acceleration(Dimension.Z))
+        basic.pause(transmitPeriod)
+    }
 })
